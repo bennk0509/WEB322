4cad20f3fb3a72e42edeaea7cc5d5f988d5005b1
bennk0509 <nkakiet21@clc.fitus.edu.vn>
2025-02-10 17:14:24 -0500

    Update server.js

--- w4-server-client/server.js
+++ w4-server-client/server.js
@@ -5,6 +5,7 @@ const HTTP_PORT = process.env.HTTP_PORT || 8080;
 const app = express();
 
 siteData.initialize().then(()=>{
+    app.use(express.static('./public'));
     app.get('/',(req,res) => {
         //res.send("Assignment 02: Khanh Anh Kiet Nguyen - 170049233");    
         res.redirect("/about");
