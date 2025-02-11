81c53f056985e108bb0cb7fe4b0a59134d18752d
bennk0509 <nkakiet21@clc.fitus.edu.vn>
2025-02-10 21:23:55 -0500

    week04 inclass assignment

--- in-class/week04/customer.js
+++ in-class/week04/customer.js
@@ -2,14 +2,30 @@ let fullName = "";
 let email = "";
 let phones = [];
 
-function getFullName()  {return fullName};
-function setFullName(name) {fullName = name;}
-
-function getEmail() {return email;}
-function setEmail() {return email;}
-
-function getPhones() {return phones;}
-function addPhone(phone) {return phones.push(phone);}
+function getFullName()
+{
+    return fullName;
+}
+function setFullName(name)
+{
+    fullName = name;
+}
+function getEmail()
+{
+    return email;
+}
+function setEmail(em)
+{
+    email = em;
+}
+function getPhones()
+{
+    return phones;
+}
+function addPhone(phone)
+{
+    phones.push(phone);
+}
 
 module.exports = {
     getFullName,
@@ -18,4 +34,10 @@ module.exports = {
     setEmail,
     getPhones,
     addPhone
-};
+}
+
+
+
+module.exports.addPhone = (phone) => {phones.push(phonne);}
+module.exports.setFullName = (name) => {fullName = name;}
+module.exports.getPhones = () => {return phones};
--- in-class/week04/myscript.js
+++ in-class/week04/myscript.js
@@ -1,13 +1,10 @@
-const customer = require('./customer.js');
+const customer = require("./customer.js");
 
 customer.setFullName("Khanh Anh Kiet Nguyen");
-customer.setEmail("kaknguyen@senecapolytechnic.ca");
-customer.addPhone("123-456-7890");
-customer.addPhone("987-654-3210");
+customer.setEmail("abcdef@gmail.com");
+customer.addPhone("123-456-789");
+customer.addPhone("789-456-123");
 
-console.log("Customer Details:");
-console.log("Full Name:", customer.getFullName());
-console.log("Email:", customer.getEmail());
-console.log("Phones:", customer.getPhones());
-
-
+console.log(customer.getFullName());
+console.log(customer.getEmail());
+console.log(customer.getPhones());
